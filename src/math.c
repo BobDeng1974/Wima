2db66e0f0c4092b2a2687856feeeb7ad56885c9b
Gavin Howard <project.lfyre@gmail.com>
2017-08-08 16:16:11 -0600

    Add a function to convert from device coordinates to pixel.

---
+++ src/math.c
@@ -0,0 +1,48 @@
+/**
+ *	***** BEGIN LICENSE BLOCK *****
+ *
+ *	Copyright 2017 Project LFyre
+ *
+ *	Licensed under the Apache License, Version 2.0 (the "Apache License")
+ *	with the following modification; you may not use this file except in
+ *	compliance with the Apache License and the following modification to it:
+ *	Section 6. Trademarks. is deleted and replaced with:
+ *
+ *	6. Trademarks. This License does not grant permission to use the trade
+ *		names, trademarks, service marks, or product names of the Licensor
+ *		and its affiliates, except as required to comply with Section 4(c) of
+ *		the License and to reproduce the content of the NOTICE file.
+ *
+ *	You may obtain a copy of the Apache License at
+ *
+ *		http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *	Unless required by applicable law or agreed to in writing, software
+ *	distributed under the Apache License with the above modification is
+ *	distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *	KIND, either express or implied. See the Apache License for the specific
+ *	language governing permissions and limitations under the Apache License.
+ *
+ *	****** END LICENSE BLOCK ******
+ *
+ *	*****************************************************************
+ *
+ *	******* BEGIN FILE DESCRIPTION *******
+ *
+ *	Source file for the math functions that Wima uses.
+ *
+ *	******** END FILE DESCRIPTION ********
+ */
+
+int wima_math_coordToPixel(int npixels, double coord) {
+
+	// TODO: I did not put much time into this.
+	// It is just a simple implementation and
+	// should be really worked later on.
+
+	int num = (npixels & 1 ? npixels : npixels - 1);
+
+	double normal = coord += 1.0;
+
+	return (int) (num * normal);
+}
