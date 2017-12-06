0d367c2038079e0248bfdc0d6109d4e3f1319696
Gavin Howard <yzena.tech@gmail.com>
2017-12-06 16:12:53 -0700

    Install xinput with setup

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-sudo apt install libx11-dev libxinerama-dev libxcursor-dev libjemalloc-dev cmake cmake-curses-gui ninja-build
+sudo apt install libx11-dev libxinerama-dev libxcursor-dev libxi-dev libjemalloc-dev cmake cmake-curses-gui ninja-build
 
 git config --local include.path ../.gitconfig
 
