dae2e783720c4872a3796cd53bc2876e15eda325
Gavin Howard <yzena.tech@gmail.com>
2017-12-06 15:26:44 -0700

    Install libxinerama-dev in setup

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-sudo apt install libx11-dev libjemalloc-dev cmake cmake-curses-gui ninja-build
+sudo apt install libx11-dev libxinerama-dev libjemalloc-dev cmake cmake-curses-gui ninja-build
 
 git config --local include.path ../.gitconfig
 
